94678e925343afee68180d772d085f92d79a0049
suwan <skdoqj@naver.com>
2024-11-03 20:59:18 +0900

    🎨 [styling] #4 카드 컴포넌트 css 진행 중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Noto_Sans_KR } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,22 +1,17 @@
+import style from "./card.module.scss";
+
 export default function Card() {
+  // 별 클릭 함수
+  // 마우스가 별에 들어올 때 실행되는 함수
+  // 마우스가 별에서 나갈 때 실행되는 함수
+
+  // 별 아이콘 렌더링 조건
+
   return (
-    <div>
-      <div>
-        {/* 마우스가 카드에 들어올 때 실행되는 함수 */}
-        {/* 마우스가 카드에서 나갈 때 실행되는 함수 */}
-
-        {/* 별 아이콘 렌더링 조건 */}
+    <>
+      <div className={`${style.card}`}>
         <img src="star.png" />
-        {/* 별 클릭 함수 */}
-        {/* 마우스가 별에 들어올 때 실행되는 함수 */}
-        {/* 마우스가 별에서 나갈 때 실행되는 함수 */}
-
-        {/* 별 아이콘 렌더링 조건 */}
         <img src="star.png" />
-        {/* 별 클릭 함수 */}
-        {/* 마우스가 별에 들어올 때 실행되는 함수 */}
-        {/* 마우스가 별에서 나갈 때 실행되는 함수 */}
-
         <a href="/desc:id">
           <div>
             <img src="칵테일 이미지" alt="Cocktail Image" />
@@ -24,15 +19,15 @@ export default function Card() {
           </div>
         </a>
 
-        {/* 하드코딩된 이미지와 칵테일 이름 예시 */}
         <div>
           <img
+            className={`${style.img}`}
             src="https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_mango_lime_virgin_margarita-1.png"
             alt="Cocktail Image"
           />
-          <h3>Cocktail</h3>
+          <h3 className={`${style.text}`}>Cocktail</h3>
         </div>
       </div>
-    </div>
+    </>
   );
 }

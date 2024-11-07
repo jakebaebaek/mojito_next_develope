54cccecc06c6fc46eace5988d1d08ef8312137a5
suwan <skdoqj@naver.com>
2024-11-07 17:28:07 +0900

    🎨 [styling] #4 헤더 퍼블리싱 완료

--- src/components/header/Herder.tsx
+++ src/components/header/Herder.tsx
@@ -1,34 +1,35 @@
 import Link from "next/link";
 import style from "./header.module.scss";
 import Person from "@public/Person.svg";
-import Search from "@public/Search2.svg";
+import Search from "@public/Search.svg";
 
 export default function Header() {
   return (
     <>
       <div className={`${style.header}`}>
-        <Link className={`${style.home_btn}`} href="/">
-          {/* <img src="" alt="Emoji" /> */}
-          <div>🍹</div>
-          <div>모히또에서 몰디브 한 잔</div>
-        </Link>
-        <ul>
-          <Link href="/find/linkTop100">
-            <li className={`${style.search_btn}`}>
-              <Search className={`${style.search_svg}`} />
-              <div>칵테일 검색</div>
-            </li>
+        <div className={`${style.nav_container}`}>
+          <Link className={`${style.home_btn}`} href="/">
+            {/* <img src="" alt="Emoji" /> */}
+            <div>🍹</div>
+            <div>모히또에서 몰디브 한 잔</div>
           </Link>
-          <Link href="/storage">
-            <li className={`${style.storage_btn}`}>내 칵테일 창고</li>
-          </Link>
-        </ul>
+          <ul>
+            <Link href="/find/linkTop100">
+              <li className={`${style.search_btn}`}>
+                <Search className={`${style.search_svg}`} />
+                <div>칵테일 검색</div>
+              </li>
+            </Link>
+            <Link href="/storage">
+              <li className={`${style.storage_btn}`}>내 칵테일 창고</li>
+            </Link>
+          </ul>
+        </div>
 
         <button className={`${style.login_btn}`}>
           <Person className={`${style.login_svg}`} />
           <div className={`${style.login_txt}`}>login</div>
         </button>
-        {/* 로그인 아이콘 */}
       </div>
     </>
   );

8a9238a9f1456494992d47ef0d2b0d8e86a05433
suwan <skdoqj@naver.com>
2024-12-06 17:06:20 +0900

    ✨ [new features] 이모지 fetch api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,12 @@ import Navigation from "@/components/common/navigation/Navigation";
 import LoginModal from "@/components/common/modal/LoginModal";
 import Top100 from "@/components/top100/Top100";
 import FilterSection from "@/components/main/FilterSection";
+import { getEmoji } from "@/lib/fetchs/fetchEmoji";
 
-export default function Home() {
+export default async function Home() {
+  const emojis = await getEmoji();
+  console.log("✅", emojis);
+
   return (
     <>
       <Navigation />

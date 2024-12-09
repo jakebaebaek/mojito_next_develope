0bdffab5f7119d732542783b767414b98c4921e4
suwan <skdoqj@naver.com>
2024-12-09 15:52:44 +0900

    ✨ [new features] 슬라이더 레벨 추가

--- src/components/main/slider/RangeSlider.tsx
+++ src/components/main/slider/RangeSlider.tsx
@@ -4,21 +4,51 @@ import { useState } from "react";
 import style from "./RangeSlider.module.scss";
 
 export default function RangeSlider() {
-  const [value, setValue] = useState(6);
+  const [value, setValue] = useState(5);
+
+  const onClickInput = (e) => {
+    console.log("클릭");
+    console.log(e);
+  };
+
+  const onClicklevel = (prop: Number) => {
+    switch (prop) {
+      case 1:
+        setValue(1);
+        break;
+      case 5:
+        setValue(5);
+        break;
+      case 10:
+        setValue(10);
+        break;
+    }
+  };
 
   return (
-    <div className={`${style.slider}`}>
-      <div
-        className={`${style.rail}`}
-        style={{ width: `${100 - (value / 10) * 100}%` }}
-      ></div>
-      <input
-        type="range"
-        min="0"
-        max="10"
-        step="2"
-        onChange={(e) => setValue(Number(e.target.value))}
-      ></input>
-    </div>
+    <>
+      <div className={`${style.slider}`} onClick={onClickInput}>
+        <div
+          className={`${style.rail}`}
+          style={{ width: `${100 - (value / 10) * 100}%` }}
+        ></div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          step="1"
+          onChange={(e) => setValue(Number(e.target.value))}
+
+          // style={{ "--thumb-left": `${value * 10}%` } as React.CSSProperties}
+        ></input>
+      </div>
+      <div className={`${style.levels}`}>
+        <span onClick={() => onClicklevel(1)} className={`${style.level_one}`}>
+          1
+        </span>
+        <span onClick={() => onClicklevel(5)}>5</span>
+        <span onClick={() => onClicklevel(10)}>10</span>
+      </div>
+    </>
   );
 }

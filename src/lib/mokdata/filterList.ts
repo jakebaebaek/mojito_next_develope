3cd7d62cd8254413e9ebec237dc678e571cecd30
suwan <skdoqj@naver.com>
2024-12-09 15:53:20 +0900

    ✨ [new features] 필터 목데이터 추가

---
+++ src/lib/mokdata/filterList.ts
@@ -0,0 +1,35 @@
+const filterList = {
+  tasting: [
+    { name: "아이셔", emoji: "Lemon" },
+    { name: "허브", emoji: "Seedling" },
+    { name: "프레시", emoji: "Droplet" },
+    { name: "아이써", emoji: "Hot Beverage" },
+    { name: "과일", emoji: "Grapes" },
+  ],
+  base: [
+    { name: "진", emoji: "Flag: United Kingdom" },
+    { name: "데킬라", emoji: "Cactus" },
+    { name: "보드카", emoji: "vodka" },
+    { name: "위스키", emoji: "Tumbler Glass" },
+    { name: "브랜디", emoji: "Wine Glass" },
+    { name: "럼", emoji: "Desert Island" },
+  ],
+  drunk: [
+    {
+      name: "술마셔도 사람구실은 해야지",
+      emoji: "Smiling Face with Smiling Eyes",
+      value: 1,
+    },
+    {
+      name: "안녕하세요 오스트랄로피테쿠스입니다",
+      emoji: "Monkey Face",
+      value: 5,
+    },
+    { name: "멍멍", emoji: "Dog Face", value: 10 },
+  ],
+  sweet_or_dry: [
+    { name: "달달함에 잠겨죽고싶다", emoji: "Honey Pot", value: 1 },
+    { name: "그냥 적당한거", emoji: "Wine Glass", value: 5 },
+    { name: "인생보다 쓴 맛", emoji: "Cigarette", value: 10 },
+  ],
+};

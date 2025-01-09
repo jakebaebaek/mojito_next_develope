29766f9e699ad75537c8c9c1de29c9d98eb610cd
jakebaebaek <jacobaek0515@daum.net>
2025-01-09 16:20:04 +0900

    ♻️ [refactor] Response => NextResponse

--- src/app/api/cocktails/route.ts
+++ src/app/api/cocktails/route.ts
@@ -1,13 +1,14 @@
 import connectDB from "@/lib/db";
 import { Cocktails } from "@/lib/schemas/cocktails";
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   try {
     connectDB();
 
     const cocktails = await Cocktails.find().lean();
-    return Response.json(cocktails);
+    return NextResponse.json(cocktails);
   } catch (error) {
-    return Response.json(error);
+    return NextResponse.json(error);
   }
 }

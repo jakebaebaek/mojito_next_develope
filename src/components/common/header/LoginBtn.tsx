ca8097c6b21fa8aa6dc115dcde948ff61d41641c
suwan <skdoqj@naver.com>
2025-01-21 23:25:07 +0900

    ✨ [new features] #29 로그인 버튼 컴포넌트 분리

---
+++ src/components/common/header/LoginBtn.tsx
@@ -0,0 +1,22 @@
+import style from "./Login.module.scss";
+import Person from "@public/Person.svg";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function LoginBtn() {
+  const { data: session } = useSession();
+  const userEmail = session?.user?.email;
+  console.log("💥", session?.user?.email);
+
+  if (session) {
+    return <button onClick={() => signOut()}>signOut</button>;
+  }
+  return (
+    <>
+      <button className={`${style.login_btn}`} onClick={() => signIn("kakao")}>
+        <Person className={`${style.login_svg}`} />
+        <div className={`${style.login_txt}`}>login</div>
+      </button>
+    </>
+  );
+}

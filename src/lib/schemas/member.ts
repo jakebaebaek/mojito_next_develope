b365277d212d86fcf55522d70edc47e176b3533e
suwan <skdoqj@naver.com>
2025-01-27 19:40:36 +0900

    ✨ [new features] #29 member store 스키마 수정: 메모 추가

--- src/lib/schemas/member.ts
+++ src/lib/schemas/member.ts
@@ -1,37 +1,59 @@
 import mongoose from "mongoose";
-const memberSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  nickname: {
-    type: String,
-    required: true,
-  },
-  provider: {
-    type: String,
-    required: true,
-  },
-});
-
-const memberStoreSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Member_list",
-    required: true,
-  },
-  store: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Cocktails",
+const memberSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
     },
-    { timestamps: true },
-  ],
-});
+    name: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+    },
+    provider: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const memberStoreSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Member_list",
+      required: true,
+    },
+    heart: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cocktails",
+        required: true,
+      },
+    ],
+    memo: [
+      {
+        cocktail_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Cocktails",
+          required: true,
+        },
+        memo_txt: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Member =
   mongoose.models?.Member_list || mongoose.model("Member_list", memberSchema);

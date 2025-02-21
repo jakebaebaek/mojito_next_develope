340703d9da2d3f70f5ce7bc9acf82316006953d4
suwan <skdoqj@naver.com>
2025-02-21 16:10:48 +0900

    ✨ [new features] #37 세션값 zustand에 추가

--- src/components/updateStore.ts
+++ src/components/updateStore.ts
@@ -1,19 +1,27 @@
 "use client";
 
 import { useMemberStore } from "@/lib/store/memberStore";
-import { addAbortListener } from "events";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function UpdateStore() {
   const { data: session, status } = useSession();
-  const { setHeart, setMemo } = useMemberStore();
+  const { setHeart, setMemo, heart, memo } = useMemberStore();
+  console.log("👽", status);
+  useEffect(() => {
+    if (session) {
+      const memberId = session.user.id;
+      const memberStore = session.user.memberStore;
+      const sessionHeart = session.user.memberStore.heart;
+      const sessionMemo = session.user.memberStore.memo;
 
-  if (session) {
-    const a = session.user;
-    console.log("🥶", status);
-    // setHeart(.heart);
-    // setMemo(session.user.memo);
-  }
+      setHeart(sessionHeart);
+      setMemo(sessionMemo);
+      console.log("🥸", session.user);
+      console.log("🥶", memberStore);
+      console.log("😍", heart);
+    }
+  }, [session]);
 
   return null;
 }

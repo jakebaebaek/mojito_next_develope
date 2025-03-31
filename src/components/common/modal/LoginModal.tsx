8474dfd29302c37290d43169bfa063d2bd765542
jakebaebaek <jacobaek0515@daum.net>
2025-03-31 17:06:48 +0900

    🔧 [modify] 카카오 로그인 이미지 삽입 및 로그인 onClick 추가

--- src/components/common/modal/LoginModal.tsx
+++ src/components/common/modal/LoginModal.tsx
@@ -1,5 +1,8 @@
 import style from "./LoginModal.module.scss";
 import Close from "@public/Close.svg";
+import KakaoLoginButton from "@public/kakao_login.png";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 type LoginModalProps = {
   onClose: () => void;
@@ -13,10 +16,17 @@ export default function LoginModal({ onClose }: LoginModalProps) {
         onClick={(e) => e.stopPropagation()} // 모달 내부 클릭 시 닫히지 않게
       >
         <div>로그인이 필요한 서비스 입니다</div>
-        <img src="kakao_login.png" />
-        <div onClick={onClose} className={style.close_button}>
-          <Close className={style.close} />
-        </div>
+
+        <Image
+          src={KakaoLoginButton.src}
+          alt="Kakao Login"
+          onClick={() => signIn("kakao")}
+          width={200} // Replace with the actual width of your image
+          height={50} // Replace with the actual height of your image
+        />
+      </div>
+      <div onClick={onClose} className={style.close_button}>
+        <Close className={style.close} />
       </div>
     </div>
   );

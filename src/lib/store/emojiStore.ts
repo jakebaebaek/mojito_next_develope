1dc114b06ac634dc53681b8bb5beca2aab2ea3f0
suwan <skdoqj@naver.com>
2025-04-21 16:10:04 +0900

    ✨ [new features] #52 emoji 서버사이드 페칭

--- src/lib/store/emojiStore.ts
+++ src/lib/store/emojiStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { getEmoji } from "../fetchs/fetchEmoji";
+import { TEmoji } from "@/lib/types/Temoji";
 
 type TEmojiStore = {
-  emojiList: any[];
+  emojiList: TEmoji[];
+  setEmoji: (data: TEmoji[]) => void;
   fetchEmoji: () => Promise<void>;
 };
 
@@ -11,10 +13,12 @@ export const useEmojiStore = create<TEmojiStore>()(
   persist(
     (set, get) => ({
       emojiList: [],
-
+      setEmoji: (data) => {
+        set({ emojiList: data });
+      },
       fetchEmoji: async () => {
         const current = get().emojiList;
-        if (current.length > 0) return; 
+        if (current.length > 0) return;
 
         const emojiList = await getEmoji();
         set({ emojiList });
@@ -28,4 +32,3 @@ export const useEmojiStore = create<TEmojiStore>()(
 function get() {
   throw new Error("Function not implemented.");
 }
-

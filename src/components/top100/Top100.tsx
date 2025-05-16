30fe255f3bd423ad958987dcb7157885e9ec811f
jakebaebaek <jacobaek0515@daum.net>
2025-05-17 02:02:49 +0900

    🎨 [styling] top100 스와이퍼 적용 및 scss 조정

--- src/components/top100/Top100.tsx
+++ src/components/top100/Top100.tsx
@@ -1,5 +1,3 @@
-// ✅ 핵심 로직: 랜덤 칵테일을 고정하고, 순환 슬라이드 구성 + 가운데 카드 강조
-
 "use client";
 
 import style from "./Top100.module.scss";
@@ -8,8 +6,7 @@ import RightSlide from "@public/LeftSlide.svg";
 import Image from "next/image";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper/modules";
-import { useSwiper } from "swiper/react";
+import { Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -18,22 +15,18 @@ import { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useCocktailStore } from "@/lib/store/cocktailStore";
 import { TCocktail } from "@/lib/types/TCocktail";
+import swiper from "swiper";
 
 export default function Top100() {
   const { cocktailList } = useCocktailStore();
-  const visibleCount = 5;
   const router = useRouter();
-  const cardWidthRem = 20; // 카드 하나의 너비 (rem 단위)
   const [realIndex, setRealIndex] = useState(0);
-
-  // ✅ 한 번만 실행되는 랜덤 칵테일 저장
   const randomCocktailsRef = useRef<TCocktail[]>([]);
-
+  const [swiper, setSwiper] = useState<swiper>();
   if (randomCocktailsRef.current.length === 0 && cocktailList.length > 0) {
     const top100Cocktails = cocktailList.filter(
       (item) => item.hashtag?.length !== 0
     );
-    console.log("해시태그 있는거", top100Cocktails);
     const indexes = Array.from(
       new Set(
         Array.from({ length: 15 }, () =>
@@ -45,50 +38,22 @@ export default function Top100() {
       .slice(0, 10);
     randomCocktailsRef.current = indexes.map((i) => top100Cocktails[i]);
   }
-
-  // const [startIndex, setStartIndex] = useState(0);
-  // const handleNext = () => {
-  //   setStartIndex((prev) => (prev + 1) % randomCocktailsRef.current.length);
-  // };
-
-  // const handlePrev = () => {
-  //   setStartIndex(
-  //     (prev) =>
-  //       (prev - 1 + randomCocktailsRef.current.length) %
-  //       randomCocktailsRef.current.length
-  //   );
-  // };
   const handleToTop100 = () => {
     router.push("/find");
   };
-  function LeftSlideControl() {
-    const swiper = useSwiper();
-    return (
-      <div className={style.slide_btn} onClick={() => swiper.slidePrev()}>
-        <LeftSlide className={style.left_svg} />
-      </div>
-    );
-  }
-  function RightSlideControl() {
-    const swiper = useSwiper();
-    return (
-      <div className={style.slide_btn} onClick={() => swiper.slideNext()}>
-        <RightSlide className={style.left_svg} />
-      </div>
-    );
-  }
-  // 슬라이드 순환 배열 만들기
-  // const visibleSlides = Array.from({ length: visibleCount }, (_, i) => {
-  //   const index = (startIndex + i) % randomCocktailsRef.current.length;
-  //   return { ...randomCocktailsRef.current[index], index };
-  // });
-
   return (
     <div className={style.top100_section}>
       <h1 className={style.title}>
         World’s Top 100 <br /> Cocktails
       </h1>
       <div className={style.slide_area}>
+        <div className={style.slide_btn}>
+          <LeftSlide
+            onClick={() => swiper?.slidePrev()}
+            className={style.left_svg}
+          />
+        </div>
+
         <Swiper
           slidesPerView={5}
           spaceBetween={30}
@@ -98,24 +63,24 @@ export default function Top100() {
           }}
           modules={[Pagination]}
           className={`${style.top100_swiper}`}
-          onSlideChange={(e) => (
+          onSlideChange={(e) =>
             setRealIndex(
               e.realIndex + 2 > randomCocktailsRef.current.length - 1
                 ? e.realIndex - (randomCocktailsRef.current.length - 2)
                 : e.realIndex + 2
-            ),
-            console.log(e.realIndex, randomCocktailsRef.current.length)
-          )}
+            )
+          }
+          onSwiper={(swiper) => setSwiper(swiper)}
         >
-          <div className={style.slide_btn}>
-            <LeftSlideControl />
-          </div>
           <div className={`${style.slider_wrap}`}>
             {randomCocktailsRef.current ? (
               randomCocktailsRef.current.map((cocktail, i) => (
                 <SwiperSlide
                   key={i}
-                  className={`${i === realIndex ? style.active : ""}`}
+                  className={`${
+                    i === realIndex ? style.active : style.non_active
+                  }`}
+                  onClick={() => router.push(`/desc/${cocktail._id}`)}
                 >
                   <div className={`${style.cocktailBox}`}>
                     <Image
@@ -136,7 +101,10 @@ export default function Top100() {
           </div>
         </Swiper>
         <div className={style.slide_btn}>
-          <RightSlideControl />
+          <RightSlide
+            onClick={() => swiper?.slideNext()}
+            className={style.right_svg}
+          />
         </div>
       </div>
       <div onClick={handleToTop100} className={`${style.find_top100_btn}`}>

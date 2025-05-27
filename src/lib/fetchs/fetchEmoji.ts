9aa6a4c1b3c9bad23cb472c7f3272235d5621c3c
jakebaebaek <jacobaek0515@daum.net>
2025-05-27 20:03:24 +0900

    💥 [conflict] fetchEmoji 주소 수정

--- src/lib/fetchs/fetchEmoji.ts
+++ src/lib/fetchs/fetchEmoji.ts
@@ -1,9 +1,12 @@
 export const getEmoji = async () => {
   try {
-    const emojis = await fetch(`/api/emoji`);
+    const emojis = await fetch(`${process.env.SERVER_URL}/api/emoji`);
+    
     return emojis.json();
   } catch (error) {
     console.error("🚨이모지 에러:", error);
     throw error;
   }
 };
+const isServer = typeof window === "undefined";
+console.log(isServer ? "서버에서 실행 중" : "클라이언트에서 실행 중");

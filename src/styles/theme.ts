151b72e86c29c4f02df1ae6ea851d95f4dcfd5ff
jakebaebaek <jacobaek0515@daum.net>
2025-06-17 01:28:12 +0900

    ➕ [add]  #52 theme 파일 생성

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+mainColor: "#ff9922",
+lightMainColor: "#ffb380",
+gray100: "#ebebeb",
+gray200: "#cccccc",
+gray300: "#a0a0a0",
+gray400: "#717171",
+gray500: "#191919",
+
+blue100: "#d8e0ff",
+red100: "#ffd4d4",
+
+//font
+light: 300,
+regular: 400,
+medium: 500,
+bold: 700,
+
+//반응형 width
+mobile: "575px",
+tablet: "720px",
+pc: "1024px",
+}

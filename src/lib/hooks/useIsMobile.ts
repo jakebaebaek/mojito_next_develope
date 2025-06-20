3e9bc859c32e832e5cca250b3c2687ac00cc699d
suwan <skdoqj@naver.com>
2025-06-21 03:01:16 +0900

    ✨ [new features] #88 모바일 훅 조건 추가

--- src/lib/hooks/useIsMobile.ts
+++ src/lib/hooks/useIsMobile.ts
@@ -3,10 +3,17 @@ import { useEffect, useState } from "react";
 export default function useIsMobile() {
   const [isMobile, setIsMobile] = useState(false);
 
+  const checkMobile = () => {
+    if (
+      /Android|iPhone|iPod|Mobile/i.test(navigator.userAgent) ||
+      window.matchMedia("(max-width: 575px)").matches
+    )
+      setIsMobile(true);
+  };
+
   useEffect(() => {
-    setIsMobile(/Android|iPhone|iPod|Mobile/i.test(navigator.userAgent));
+    checkMobile();
   }, []);
 
-  console.log("😈", isMobile);
   return isMobile;
 }

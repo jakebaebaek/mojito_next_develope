d17da39196298f4a9e5b571f0a0220ea0c132c4a
jakebaebaek <jacobaek0515@daum.net>
2025-06-23 05:40:18 +0900

    🚑 [hotfix] 로그아웃 후 재로그인시 프로필이미지 뜨지 않던 오류 수정

--- src/lib/fetchs/fetchProfile.ts
+++ src/lib/fetchs/fetchProfile.ts
@@ -1,5 +1,5 @@
 
-export const postProfile = async (nickname: string, profileImage: string) => {
+export const postProfile = async (nickname: string, profileImage: string | undefined) => {
   try {
     const response = await fetch(`/api/profile`, {
       method: "POST",
@@ -7,6 +7,21 @@ export const postProfile = async (nickname: string, profileImage: string) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({nickname, profileImage}),
+    });
+    return response.json();
+  } catch (error) {
+    console.error("닉네임 post 에러:", error);
+    throw error;
+  }
+};
+
+export const getProfile = async() => {
+  try{
+    const response = await fetch(`/api/profile`, {
+      method:"GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
     return response.json();
   } catch (error) {

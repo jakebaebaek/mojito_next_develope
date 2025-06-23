91d3f5de49abd492bb090b62e7950ca04d82c0b0
suwan <skdoqj@naver.com>
2025-06-24 00:12:26 +0900

    🔧 [modify] #52 필터 label 수정

--- src/lib/mokdata/filterData.ts
+++ src/lib/mokdata/filterData.ts
@@ -9,16 +9,16 @@ export const filterData = {
   ],
   booziness: [
     {
-      name: "논알콜",
+      name: "가볍게",
       emoji: "Smiling Face with Smiling Eyes",
       value: 0,
     },
-    { name: "Medium", emoji: "Monkey Face", value: 5 },
+    { name: "좋은 기분", emoji: "Monkey Face", value: 5 },
     { name: "멍멍", emoji: "Dog Face", value: 10 },
   ],
   sweetness: [
-    { name: "Sweet", emoji: "Honey Pot", value: 0 },
-    { name: "Medium", emoji: "Wine Glass", value: 5 },
-    { name: "Dry", emoji: "Cigarette", value: 10 },
+    { name: "스윗", emoji: "Honey Pot", value: 0 },
+    { name: "달콤쌉쌀", emoji: "Wine Glass", value: 5 },
+    { name: "드라이", emoji: "Cigarette", value: 10 },
   ],
 };

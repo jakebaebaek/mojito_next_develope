ada0ce6b0008d426c7471aee0e4df40aeaa82d84
suwan <skdoqj@naver.com>
2025-07-03 22:14:55 +0900

    🔧 [modify] fetch env로 수정

--- src/lib/fetchs/fetchRating.ts
+++ src/lib/fetchs/fetchRating.ts
@@ -1,17 +1,22 @@
-
-export const postRating = async (cocktailId: string | string[], rating: number) => {
+export const postRating = async (
+  cocktailId: string | string[],
+  rating: number
+) => {
   try {
-    const response = await fetch(`http://localhost:3000/api/review/rating`, {
-      method: 'POST',
-      cache : "no-store",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ cocktailId, rating }),
-    });
+    const response = await fetch(
+      `${process.env.SERVER_URL}/api/review/rating`,
+      {
+        method: "POST",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ cocktailId, rating }),
+      }
+    );
 
     if (!response.ok) {
-      throw new Error('리뷰 저장에 실패했습니다.');
+      throw new Error("리뷰 저장에 실패했습니다.");
     }
     const data = await response.json();
     return data;
@@ -22,17 +27,20 @@ export const postRating = async (cocktailId: string | string[], rating: number) 
 };
 export const getRating = async (cocktailId: string | string[]) => {
   try {
-    const response = await fetch(`http://localhost:3000/api/review/rating?cocktailId=${cocktailId}`, {
-      method: 'GET',
-      cache : "no-store",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      `${process.env.SERVER_URL}/api/review/rating?cocktailId=${cocktailId}`,
+      {
+        method: "GET",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     if (!response.ok) {
-      throw new Error('리뷰 불러오기에 실패했습니다.');
+      throw new Error("리뷰 불러오기에 실패했습니다.");
     }
-    return response.json(); 
+    return response.json();
   } catch (error) {
     console.error("리뷰 불러오기 에러:", error);
     throw error;
